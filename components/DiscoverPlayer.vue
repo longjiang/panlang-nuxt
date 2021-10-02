e4d8dba263b039b8b5a829e75485c21380189373
Jiang Long <longjiang2005@gmail.com>
2021-10-02 15:14:12 -0700

    Refactor and move 'discover player' to its own component.

---
+++ components/DiscoverPlayer.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="widget widget-dark mb-5" style="max-width: 70vh; margin: 0 auto">
+    <div class="widget-title">
+      Discover {{ $l2.name }}
+      {{ routeType === "tv-shows" ? "TV Shows" : "Talks" }}
+    </div>
+    <div class="text-center pt-5 pb-5" v-if="!randomShowFirstEpisode">
+      <Loader :sticky="true" message="Getting shows..." />
+    </div>
+    <LazyYouTubeVideo
+      v-if="randomShowFirstEpisode"
+      initialLayout="vertical"
+      :youtube="randomShowFirstEpisode.youtube_id"
+      :ref="`youtube`"
+      :autoload="true"
+      :autoplay="true"
+      :startAtRandomTime="true"
+      @currentTime="updateCurrentTime"
+    />
+    <div class="text-center pt-3 pb-3" v-if="randomShowFirstEpisode">
+      <router-link
+        :to="{
+          name: 'youtube-view',
+          params: {
+            youtube_id: randomShowFirstEpisode.youtube_id,
+          },
+          query: {
+            t: currentTime,
+          },
+        }"
+        class="btn btn-ghost-dark-no-bg"
+      >
+        <i class="fas fa-align-left mr-1"></i>
+        Transcript
+      </router-link>
+      <b-button variant="ghost-dark-no-bg" @click="loadRandomShow">
+        <i class="fas fa-step-forward mr-1"></i>
+        Another One
+      </b-button>
+      <b-button
+        variant="ghost-dark-no-bg"
+        v-if="$adminMode"
+        @click="removeEpisode(randomShowFirstEpisode)"
+      >
+        <i class="fas fa-trash"></i>
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Config from "@/lib/config";
+export default {
+  props: {
+    routeType: {
+      default: "tv-shows", // or 'talks'
+    },
+    shows: {
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      currentTime: 0,
+      randomShow: undefined,
+      randomShowId: undefined,
+      randomShowFirstEpisode: undefined,
+    };
+  },
+  computed: {
+    $l1() {
+      if (typeof this.$store.state.settings.l1 !== "undefined")
+        return this.$store.state.settings.l1;
+    },
+    $l2() {
+      if (typeof this.$store.state.settings.l2 !== "undefined")
+        return this.$store.state.settings.l2;
+    },
+    $adminMode() {
+      if (typeof this.$store.state.settings.adminMode !== "undefined")
+        return this.$store.state.settings.adminMode;
+    },
+  },
+  mounted() {
+    this.loadRandomShow();
+  },
+  watch: {
+    shows() {
+      this.loadRandomShow();
+    }
+  },
+  methods: {
+    async loadRandomShow() {
+      let randomShow = this.getRandomShow();
+      if (randomShow) {
+        let randomShowFirstEpisode = await this.getFirstEpisodeOfShow(
+          randomShow.id,
+          this.routeType.replace(/s$/, "").replace("-", "_")
+        );
+        this.randomShow = randomShow;
+        this.randomShowFirstEpisode = randomShowFirstEpisode;
+      }
+    },
+    getRandomShow() {
+      if (this.shows) {
+        let shows = this.shows.filter((s) => {
+          if (
+            this.routeType === "tv-shows" &&
+            ["Music", "Movies"].includes(s.title)
+          )
+            return false;
+          if (this.routeType === "talks" && ["News"].includes(s.title))
+            return false;
+          return true;
+        });
+        let randomShow = shows[Math.floor(Math.random() * shows.length)];
+        return randomShow;
+      }
+    },
+    updateCurrentTime(currentTime) {
+      if (typeof window !== "undefined") {
+        this.currentTime = currentTime;
+      }
+    },
+    async getFirstEpisodeOfShow(showId, showType) {
+      let url = `${Config.wiki}items/youtube_videos?filter[l2][eq]=${this.$l2.id}&filter[${showType}][eq]=${showId}&fields=youtube_id,id`;
+      let response = await axios.get(url);
+
+      if (response.data && response.data.data.length > 0) {
+        let videos = response.data.data;
+        let firstEpisode = videos[0];
+        return firstEpisode;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
