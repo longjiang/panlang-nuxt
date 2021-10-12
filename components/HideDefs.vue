5ae624265a9cabf8e4ade7b119046e717bb7ef7e
Jiang Long <longjiang2005@gmail.com>
2021-10-12 10:03:48 -0700

    Abstract "hide defs, hide phonetics" toggle to its own component.

---
+++ components/HideDefs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-form-checkbox v-model="hideDefinitions" class="d-inline">
+      Hide defs
+    </b-form-checkbox>
+    <b-form-checkbox v-model="hidePhonetics" class="ml-2 d-inline">
+      Hide phonetics
+    </b-form-checkbox>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hideDefinitions: false,
+      hidePhonetics: false,
+    };
+  },
+  watch: {
+    hideDefinitions() {
+      this.$store.commit("settings/SET_HIDE_DEFINITIONS", this.hideDefinitions);
+    },
+    hidePhonetics() {
+      this.$store.commit("settings/SET_HIDE_PHONETICS", this.hidePhonetics);
+    },
+  },
+  mounted() {
+    if (typeof this.$store.state.settings !== "undefined") {
+      this.hideDefinitions = this.$store.state.settings.hideDefinitions;
+      this.hidePhonetics = this.$store.state.settings.hidePhonetics;
+    }
+    this.unsubscribe = this.$store.subscribe((mutation, state) => {
+      if (mutation.type === "settings/LOAD_SETTINGS") {
+        this.hideDefinitions = this.$store.state.settings.hideDefinitions;
+        this.hidePhonetics = this.$store.state.settings.hidePhonetics;
+      }
+    });
+  },
+};
+</script>
+
+<style>
+</style>
